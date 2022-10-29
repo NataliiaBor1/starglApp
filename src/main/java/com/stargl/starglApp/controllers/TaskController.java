39f576a191adf4015c4e2e5c03f97068db0a577d
NataliiaBor1 <borisenko.nataliia1@gmail.com>
2022-10-28 23:57:10 -0700

    commit 6, day 5

--- src/main/java/com/stargl/starglApp/controllers/TaskController.java
+++ src/main/java/com/stargl/starglApp/controllers/TaskController.java
@@ -1,10 +1,13 @@
 package com.stargl.starglApp.controllers;
 
+import com.stargl.starglApp.dtos.TaskDto;
 import com.stargl.starglApp.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
@@ -12,6 +15,31 @@ public class TaskController {
 
     @Autowired
     private TaskService taskService;
+
+    @GetMapping("/user/{userId}")
+    public List<TaskDto> getNotesByUser(@PathVariable Long userId) {
+        return taskService.getAllTasksByUserId(userId);
+    }
+
+    @GetMapping("/{taskId}")
+    public Optional<TaskDto> getTaskById(@PathVariable Long taskId) {
+        return taskService.getTaskById(taskId);
+    }
+
+    @PostMapping("/user/{userId}")
+    public void addTask(@RequestBody TaskDto taskDto, @PathVariable Long userId) {
+        taskService.addTask(taskDto, userId);
+    }
+
+    @DeleteMapping("/{taskId}")
+    public void deleteTaskById(@PathVariable Long taskId) {
+        taskService.deleteTaskById(taskId);
+    }
+
+    @PutMapping
+    public void updateTask(@RequestBody TaskDto taskDto) {
+        taskService.updateTaskById(taskDto);
+    }
 
 
 }
